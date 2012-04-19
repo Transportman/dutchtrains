af5faf456c715107c13b43134e2cacf1fbd74fdf
FooBar <foobar@jaspervries.nl>
2012-04-19 20:02:58 +0200

    Change: use Dutch Trainset as name also when building with build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ echo Writing version info
 sed -i s/REPO_REVISION/$(hg parents --template="{rev}")/ dutchtrains.nml
 echo Writing custom_tags
 echo VERSION :r$(hg parents --template="{rev}")>custom_tags.txt
-echo TITLE :Dutch Trains r$(hg parents --template="{rev}")M>>custom_tags.txt
+echo TITLE :Dutch Trainset r$(hg parents --template="{rev}")M>>custom_tags.txt
 echo FILENAME :dutchtrains.grf>>custom_tags.txt
 echo Running NML
 nmlc -p DOS dutchtrains.nml
