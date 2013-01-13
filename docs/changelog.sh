c7624afa58c24eef0c6337d316c82c8c8db176f8
FooBar <foobar@jaspervries.nl>
2013-01-13 12:39:04 +0100

    Docs: prepare for release

--- docs/changelog.sh
+++ docs/changelog.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 #Create list of constants and variables
-hg log -r 626:tip --template '- {desc}\n' | sort > changelog_raw.txt
+hg log -r 653:tip --template '- {desc}\n' | sort > changelog_raw.txt
