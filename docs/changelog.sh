c8ff90ebdc01fb8878a6fe8f59248d63d8799e87
Transportman <47077527+Transportman@users.noreply.github.com>
2021-04-24 18:11:50 +0200

    Docs: prepare for release

--- docs/changelog.sh
+++ docs/changelog.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 #Create list of constants and variables
-hg log -r 974:tip --template '- {desc}\n' | sort > changelog_raw.txt
+hg log -r 1005:tip --template '- {desc}\n' | sort > changelog_raw.txt
